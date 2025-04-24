5f17f6c331d9d4048e689c497bbad4879702377d
Sergey Schadilov <segulka123@mail.ru>
2025-04-24 17:46:19 +0300

    First try

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,23 @@
 package service
+
+import (
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+func isMorse(in string) bool {
+	runes := []rune(in)
+	for _, char := range runes {
+		if char != '.' && char != '-' && char != ' ' {
+			return false
+		}
+	}
+	return true
+}
+
+func Convert(in string) string {
+	if isMorse(in) {
+		return morse.ToText(in)
+	} else {
+		return morse.ToMorse(in)
+	}
+}
